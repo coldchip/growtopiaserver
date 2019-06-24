294b4e08a8d2cf3b59255cd93af31ffe298e7f50
coldchip <growwick@gmail.com>
2019-06-25 00:47:40 +0800

    s

---
+++ Makefile
@@ -0,0 +1,26 @@
+JNI_COMPILER=gcc
+
+JAVA_COMPILER=javac
+
+SWIG_COMPILER=swig
+
+DISPLAY_TEXT=Compiling Files
+
+module:
+	# $(SWIG_COMPILER) -java -outdir Enet -package Enet native_enet/enet.i
+	# ^^ PLEASE DO NOT UNCOMMENT THE TOP LINE ^^ 
+	$(JNI_COMPILER) -c native_enet/enet/unix.c -fPIC -DHAS_SOCKLEN_T=1 -o native_enet/enet/unix.lo
+	$(JNI_COMPILER) -c native_enet/enet/win32.c -fPIC -o native_enet/enet/win32.lo
+	$(JNI_COMPILER) -c native_enet/enet/protocol.c -fPIC -o native_enet/enet/protocol.lo
+	$(JNI_COMPILER) -c native_enet/enet/peer.c -fPIC -o native_enet/enet/peer.lo
+	$(JNI_COMPILER) -c native_enet/enet/packet.c -fPIC -o native_enet/enet/packet.lo
+	$(JNI_COMPILER) -c native_enet/enet/list.c -fPIC -o native_enet/enet/list.lo
+	$(JNI_COMPILER) -c native_enet/enet/host.c -fPIC -o native_enet/enet/host.lo
+	$(JNI_COMPILER) -c native_enet/enet/compress.c -fPIC -o native_enet/enet/compress.lo
+	$(JNI_COMPILER) -c native_enet/enet/callbacks.c -fPIC -o native_enet/enet/callbacks.lo
+
+	$(JNI_COMPILER) -c native_enet/enet_wrap.c -o native_enet/enet_wrap.o
+	$(JNI_COMPILER) -shared native_enet/enet/*.lo native_enet/enet_wrap.o -o libenet.so
+	rm -r native_enet/enet/*.lo
+	$(JAVA_COMPILER) Server.java
+	
---
+++ Server.java
@@ -0,0 +1,46 @@
+import Enet.enet;
+import Enet.ENetHost;
+import Enet.ENetEvent;
+import Enet.ENetAddress;
+import Enet.ENetEventType;
+import Enet.ENetPacket;
+import Enet.ENetPacketFlag;
+import Enet.ENetBuffer;
+
+public class Server { 
+
+	public static void main(String[] args) {
+		try {
+    		System.load("/home/coldchip/gtserver/libenet.so");
+    		enet en = new enet();
+    		ENetEvent event = new ENetEvent();
+    		ENetAddress address = new ENetAddress();
+    		en.enet_address_set_host_ip(address, "0.0.0.0");
+    		address.setPort(10003);
+    		ENetHost host = en.enet_host_create(address, 3200, 2, 0, 0);
+    		en.set_crc32(host);
+    		en.enet_host_compress_with_range_coder(host);
+    		while(true) {
+				if(en.enet_host_service(host, event, 100) > 0) {
+					ENetEventType type = event.getType();
+					if(type == ENetEventType.ENET_EVENT_TYPE_CONNECT) {
+						System.out.println(event.getPeer());
+						byte[] connectByte = new byte[] {0x01, 0x00, 0x00, 0x00, 0x00};
+						ENetPacket packet = en.enet_packet_create(connectByte, ENetPacketFlag.ENET_PACKET_FLAG_RELIABLE);
+						en.enet_peer_send(event.getPeer(), (short)0, packet);
+					}
+					if(type == ENetEventType.ENET_EVENT_TYPE_RECEIVE) {
+
+					}
+					if(type == ENetEventType.ENET_EVENT_TYPE_DISCONNECT) {
+						
+					}
+				}
+    		}
+	    } catch (UnsatisfiedLinkError e) {
+			System.err.println("Native code library failed to load.\n" + e);
+			System.exit(1);
+	    }
+	}
+
+}
