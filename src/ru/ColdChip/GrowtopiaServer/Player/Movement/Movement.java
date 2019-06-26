718a8c3afa990172fbbfdeeaffef5d0044e7fb47
coldchip <growwick@gmail.com>
2019-06-26 14:41:48 +0800

    Update

--- src/ru/ColdChip/GrowtopiaServer/Player/Movement/Movement.java
+++ src/ru/ColdChip/GrowtopiaServer/Player/Movement/Movement.java
@@ -2,9 +2,32 @@ package ru.ColdChip.GrowtopiaServer.Player.Movement;
 
 import ru.ColdChip.GrowtopiaServer.Player.Movement.Structs.MovementData;
 import ru.ColdChip.GrowtopiaServer.Packet.Structs.PacketData;
+import java.util.Arrays;
 
 public class Movement {
 	public MovementData unpackMovement(byte[] data) {
-		return new MovementData();
+		MovementData movementData = new MovementData();
+		movementData.packetType = (Byte2Int(Arrays.copyOfRange(data, 0, 4)));
+		movementData.netID = (Byte2Int(Arrays.copyOfRange(data, 4, 8)));
+		movementData.characterState = (Byte2Int(Arrays.copyOfRange(data, 12, 16)));
+		movementData.plantingTree = (Byte2Int(Arrays.copyOfRange(data, 20, 24)));
+		movementData.x = (Float.intBitsToFloat(Byte2Int(Arrays.copyOfRange(data, 24, 28))));
+		movementData.y = (Float.intBitsToFloat(Byte2Int(Arrays.copyOfRange(data, 28, 32))));
+		movementData.XSpeed = (Float.intBitsToFloat(Byte2Int(Arrays.copyOfRange(data, 32, 36))));
+		movementData.YSpeed = (Float.intBitsToFloat(Byte2Int(Arrays.copyOfRange(data, 36, 40))));
+		movementData.punchX = (Byte2Int(Arrays.copyOfRange(data, 44, 48)));
+		movementData.punchY = (Byte2Int(Arrays.copyOfRange(data, 48, 52)));
+		return movementData;
+	}
+
+	private int Byte2Int(byte[] bytes) {
+
+		int asInt = (bytes[0] & 0xFF) 
+		| ((bytes[1] & 0xFF) << 8) 
+		| ((bytes[2] & 0xFF) << 16) 
+		| ((bytes[3] & 0xFF) << 24);
+
+		return asInt;
+
 	}
 }
--- src/ru/ColdChip/GrowtopiaServer/Player/Movement/Structs/MovementData.java
+++ src/ru/ColdChip/GrowtopiaServer/Player/Movement/Structs/MovementData.java
@@ -1,5 +1,14 @@
 package ru.ColdChip.GrowtopiaServer.Player.Movement.Structs;
 
 public class MovementData {
-	
+	public int packetType;
+	public int netID;
+	public float x;
+	public float y;
+	public int characterState;
+	public int plantingTree;
+	public float XSpeed;
+	public float YSpeed;
+	public int punchX;
+	public int punchY;
 }
